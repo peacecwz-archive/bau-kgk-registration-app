3dd7d5d95cb52a5cd1a3cff382bfd67a1f75a316
Barış Ceviz <me@barisceviz.com>
2018-06-01 04:37:21 +0300

    Added Member model

---
+++ KGKRegister/Models/Member.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace KGKRegister
+{
+    public class Member
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentNumber
+        {
+            get;
+            set;
+        }
+        public string Departmant
+        {
+            get;
+            set;
+        }
+        public string Faculty
+        {
+            get;
+            set;
+        }
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+        public bool IsPaid
+        {
+            get;
+            set;
+        }
+        public DateTime CreatedOn
+        {
+            get;
+            set;
+        }
+    }
+}
